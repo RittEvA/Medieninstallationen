51346dd574aa751b674591576bc31ace42ceaf0c
RittEvA <44029211+RittEvA@users.noreply.github.com>
2021-05-26 09:38:07 +0200

    test

--- js/main.ts
+++ js/main.ts
@@ -1,3 +1,17 @@
 namespace Medieninstallationen {
-  
+    document.getElementById("back").addEventListener("click", getLocation);
+    var x = document.getElementById("demo");
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position:any) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+    }
 }
