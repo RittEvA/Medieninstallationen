15531b93e58b676ffad11e657c35bb6b29b6502f
RittEvA <44029211+RittEvA@users.noreply.github.com>
2021-06-03 09:57:31 +0200

    try

--- js/main.js
+++ js/main.js
@@ -2,14 +2,23 @@ var Medieninstallationen;
 (function (Medieninstallationen) {
     document.getElementById("start").addEventListener("click", start);
     document.getElementById("stop").addEventListener("click", stop);
+    navigator.geolocation.getCurrentPosition(getLocation, zeigeFehler); //Für Positionsabfrage
+    navigator.geolocation.watchPosition(getLocation, zeigeFehler); //Für Verfolgung
     var x = document.getElementById("demo");
     function start() {
-        alert("hier bin ich");
+        //alert("hier bin ich");
         setInterval(getLocation, 100);
     }
+    /* function getLocation() {//frägt den aktuellen Standort ab
+         if (navigator.geolocation) {
+             navigator.geolocation.getCurrentPosition(showPosition);
+         } else {
+             x.innerHTML = "Geolocation is not supported by this browser.";
+         }
+     }*/
     function getLocation() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
+            navigator.geolocation.watchPosition(showPosition);
         }
         else {
             x.innerHTML = "Geolocation is not supported by this browser.";
@@ -21,6 +30,22 @@ var Medieninstallationen;
     }
     function stop() {
         clearInterval();
+    }
+    function zeigeFehler(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                x.innerHTML = "Benutzer lehnte Standortabfrage ab.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "Standortdaten sind nicht verfügbar.";
+                break;
+            case error.TIMEOUT:
+                x.innerHTML = "Die Standortabfrage dauerte zu lange (Time-out).";
+                break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML = "unbekannter Fehler.";
+                break;
+        }
     }
 })(Medieninstallationen || (Medieninstallationen = {}));
 //# sourceMappingURL=main.js.map
--- js/main.ts
+++ js/main.ts
@@ -1,26 +1,53 @@
 namespace Medieninstallationen {
     document.getElementById("start").addEventListener("click", start);
     document.getElementById("stop").addEventListener("click", stop);
+    navigator.geolocation.getCurrentPosition(getLocation, zeigeFehler); //Für Positionsabfrage
+    navigator.geolocation.watchPosition(getLocation, zeigeFehler);  //Für Verfolgung
 
     var x = document.getElementById("demo");
 
-    function start(){
-        alert("hier bin ich");
+    function start(){//startet Intervall damit in regelmäßigen Abständen die GPS-Daten überprüft werden
+        //alert("hier bin ich");
         setInterval(getLocation, 100);
     }
-    function getLocation() {
+   /* function getLocation() {//frägt den aktuellen Standort ab
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition);
         } else {
             x.innerHTML = "Geolocation is not supported by this browser.";
         }
-    }
+    }*/
+    function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.watchPosition(showPosition);
+        } else {
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+      }
 
-    function showPosition(position:any) {
+    function showPosition(position:any) {// schreibt die Koordinaten ins HTML
         x.innerHTML = "Latitude: " + position.coords.latitude +
             "<br>Longitude: " + position.coords.longitude;
     }
     function stop(){
         clearInterval();
     }
+
+
+function zeigeFehler(error:any) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "Benutzer lehnte Standortabfrage ab."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Standortdaten sind nicht verfügbar."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "Die Standortabfrage dauerte zu lange (Time-out)."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "unbekannter Fehler."
+            break;
+    }
+}
 }
